4f8a9089fbef6f13f0cf0a47976469de8c4d7283
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-04-26 10:58:57 +0100

    carousel images updated, nav position fixed

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Carousel = () => {
   return (
     <>
-        <div id="carouselExampleCaptions" className="carousel slide">
+        <div id="carouselExampleCaptions" className="carousel slide mt-5 ">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -11,21 +11,21 @@ const Carousel = () => {
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src="..." className="d-block w-100" alt="..."/>
+      <img src="./assets/LJ13.PNG" className="d-block w-100  h-75" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
+      <img src="./assets/LJ10.PNG" className="d-block w-100 h-75" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
+      <img src="./assets/LJ11.PNG" className="d-block w-100 h-75" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>

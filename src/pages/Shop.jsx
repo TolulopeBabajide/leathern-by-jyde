037a9604d8c5b5066cade3b0a6d28154eb3d75f7
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-04-26 11:01:27 +0100

    shop page updated

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,13 +2,51 @@ import React from 'react'
 import Nav from '../components/navbar/Nav'
 import Footer from '../components/footer/Footer'
 
-const Featured = () => {
+const Shop = () => {
   return (
     <>
         <Nav/>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="./assets/LJ60.PNG" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./assets/LJ70.PNG" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./assets/LJ80.PNG" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./assets/LJ90.PNG" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
         <Footer/>
     </>
   )
 }
 
-export default Featured
+export default Shop
